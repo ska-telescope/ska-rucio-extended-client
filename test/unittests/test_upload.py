7edd9374887646cdcabde0be24026be4e9da4669
Rob Barnsley <R.Barnsley@skatelescope.org>
2022-10-04 12:11:45 +0100

    nt

--- test/unittests/test_upload.py
+++ test/unittests/test_upload.py
@@ -16,6 +16,7 @@ class TestUploadFolder:
 
     def test_upload_folder_add_steps(self):
         """ Checking addition of steps to plan. Do this by checking the number of steps per section. """
+        self.plan.describe()
         for section in self.plan.sections:
             if section == 'create_collections':
                 assert len([step for step in self.plan.steps if step.section_name == section]) == 11
@@ -23,5 +24,5 @@ class TestUploadFolder:
                 assert len([step for step in self.plan.steps if step.section_name == section]) == 10
             elif section == 'add_metadata':
                 assert len([step for step in self.plan.steps if step.section_name == section]) == 3
-        self.plan.describe()
+
 
