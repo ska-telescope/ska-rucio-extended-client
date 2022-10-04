accbfe4f6a8d314f6120057cbcf85ea0f39aa0bb
Rob Barnsley <R.Barnsley@skatelescope.org>
2022-10-04 12:07:31 +0100

    add test dir

---
+++ test/run_unittests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /opt/rucio-extended-client
+python3.6 -m pytest test/unittests
---
+++ test/unittests/test_upload.py
@@ -0,0 +1,26 @@
+import os
+
+from rucio_extended_client.api.plan import UploadPlan
+
+class TestUploadFolder:
+    plan = UploadPlan.make_plan_from_directory(
+        root_directory=os.path.join('test', 'data', 'dummy_directory'),
+        root_container_name='test',
+        rse='test_rse',
+        scope='test_scope',
+        lifetime=3600,
+        root_suffix='__root',
+        path_delimiter='.',
+        mock=True
+    )
+
+    def test_upload_folder_add_steps(self):
+        """ Checking addition of steps to plan. Do this by checking the number of steps per section. """
+        for section in self.plan.sections:
+            if section == 'create_collections':
+                assert len([step for step in self.plan.steps if step.section_name == section]) == 11
+            elif section == 'create_attachments':
+                assert len([step for step in self.plan.steps if step.section_name == section]) == 10
+            elif section == 'add_metadata':
+                assert len([step for step in self.plan.steps if step.section_name == section]) == 3
+
